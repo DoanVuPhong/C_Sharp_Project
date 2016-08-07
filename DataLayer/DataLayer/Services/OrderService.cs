5a86abaaa1bc5b321f112036c1a46ac3f4ae5789
nghiantse61642 <nghiantse61642@fpt.edu.vn>
2016-08-07 16:38:53 +0700

    [Add-Order] Add order record to database.[NghiaNT]

--- DataLayer/DataLayer/Services/OrderService.cs
+++ DataLayer/DataLayer/Services/OrderService.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace DataLayer.Services
 {
-    class OrderService
+    public class OrderService
     {
+        public static bool Add(Order o)
+        {
+            try
+            {
+                using (Book_Sale_ManagerEntities context = new Book_Sale_ManagerEntities())
+                {
+                    context.Orders.Add(o);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Can't add order to database. Detail: {0}", ex);
+            }
+            return false;
+        }
+
     }
 }
