ebffa6f4f8dccd5268069057019f857379cea9c1
phongdv <vuphongkuj1@gmail.com>
2016-08-09 17:14:27 +0700

    [Logservice] phongdv Add Logservice to Project

--- DataLayer/DXApp/AuthorForm.cs
+++ DataLayer/DXApp/AuthorForm.cs
@@ -26,11 +26,18 @@ namespace DXApp
             ChannelFactory<IBussinessLogic> chanel = new ChannelFactory<IBussinessLogic>("ClientEndPoint");
             proxy = chanel.CreateChannel();
             getDataScource();
-            setCurrent();
-            current.ID = (int)table.Rows[0][0];
+         
+            if (table.Rows.Count > 0)
+            {
+                setCurrent();
+                current.ID = (int)table.Rows[0][0];
+
+            }
+
         }
 
-        void setCurrent() {
+        void setCurrent()
+        {
             if (dgv.SelectedRows.Count > 0)
             {
 
@@ -110,11 +117,11 @@ namespace DXApp
             {
                 getDataScource();
                 MessageBox.Show("Delete Author Successful!");
-                
+
             }
             else
             {
-                MessageBox.Show("Delete Fail! Please Try Again");
+                MessageBox.Show("Can not Delete this Author, Because This author is used by some Books, If you want to delete this Author please remove all book related to this author!");
 
             }
         }
@@ -128,7 +135,7 @@ namespace DXApp
             {
                 getDataScource();
                 MessageBox.Show("Update Author Successful!");
-                
+
             }
             else
             {
