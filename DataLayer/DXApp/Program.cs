85e2ed92dae822d84d48d20056ee4b337cba856f
phongdv <vuphongkuj1@gmail.com>
2016-08-11 14:09:15 +0700

    save change merge

--- DataLayer/DXApp/Program.cs
+++ DataLayer/DXApp/Program.cs
@@ -22,7 +22,7 @@ namespace DXApp
             BonusSkins.Register();
             SkinManager.EnableFormSkins();
             UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
-            Application.Run(new AddBook());
+            Application.Run(new Form1());
         }
     }
 }
